c1f0b4153efb557710d6228a957efaf0932e7b9e
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-03-20 20:58:06 -0400

    Working on ts project setting it up

---
+++ backend/routes/clientpreference.route.ts
@@ -0,0 +1,11 @@
+import { Router, Request, Response } from 'express';  
+import { clientPrefController } from '../controllers/clientPref.controller.js'; 
+// instantiate the route here as such 
+const clientPref = Router();    
+
+
+clientPref.get('/clientpreference', clientPrefController); 
+
+export default clientPref; 
+
+
