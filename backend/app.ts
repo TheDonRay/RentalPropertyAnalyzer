7fbf5a03e7df267628f8e5c344180b4dbf40bf2c
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-03-20 21:07:31 -0400

    set up docker

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,15 @@
 import express from 'express'; 
-const app = express(); 
+const app = express();  
+
+// import routes 
+import clientPref from './routes/clientpreference.route.js'; 
 
 //set up some middleware here as such 
-app.use(express.json()); 
+app.use(express.json());  
+
+
+//instantiate the route here as such 
+app.use('/api/v1/', clientPref); 
 
 // send a set up response as such 
 app.get('/', (req, res) => { 
