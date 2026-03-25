353ab71c567b0086ea32aee8ae7ba34644960dbf
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-03-25 14:40:06 -0400

    working on the backend

--- backend/controllers/clientPref.controller.ts
+++ backend/controllers/clientPref.controller.ts
@@ -1,21 +1,23 @@
 import { Request, Response } from "express";
-// import database as well 
+// import database as well
 
-// import external service function here as such -> define the service function. 
-import {externalapifunction}  from '../services/externalapifunction.service.js'
+// import external service function here as such -> define the service function.
+import { externalapifunction } from "../services/externalapifunction.service.js";
 
 const clientpropdataController = async (req: Request, res: Response) => {
-  // get user address data here as such 
-  try { 
-    const address = req.body;  
-    if (!address || address.length === 0) { 
-      
+  // get user address data here as such
+  try {
+    const address = req.body;
+    // research better way to validate addresses.
+    if (!address || address.length === 0) {
+      throw new Error("Error not valid address");
     }
-
-  } catch (error) { 
-
+    console.log("valid address");
+    // call the function here
+    externalapifunction(address);
+  } catch (error) {
+    console.error("There was an error reading your address", error);
   }
-  // call the user function here as such 
 };
 
 export default clientpropdataController;
--- backend/services/externalapifunction.service.ts
+++ backend/services/externalapifunction.service.ts
@@ -1,27 +1,27 @@
-interface clientAddress { 
-    UserAddress: string 
-};  
+interface clientAddress {
+  UserAddress: string;
+}
 
-
-const externalapifunction = async (Address : clientAddress) => { 
-    // handle cases here create try and catch case as such 
-    try { 
-        if (!Address || Address.UserAddress.length === 0) { 
-            throw new Error('Error no address entered');   
-        } 
-        console.log('Address entered');  
-        // build the external function here as such 
-        const buildingURL = new URLSearchParams({ 
-            address: Address.UserAddress
-        });  
-
-        const externalapiURL = `https://api.rentcast.io/v1/properties?${buildingURL}` 
-        console.log(externalapiURL); 
-        return; 
-    } catch (error) { 
-        console.error('Error', error);  
-        return; 
+const externalapifunction = async (Address: clientAddress) => {
+  // handle cases here create try and catch case as such
+  try {
+    // research better way to validate addresses.
+    if (!Address || Address.UserAddress.length === 0) {
+      throw new Error("Error no address entered");
     }
-}; 
+    console.log("Address entered");
+    // build the external function here as such
+    const buildingURL = new URLSearchParams({
+      address: Address.UserAddress,
+    });
 
-export default { externalapifunction }; 
+    const externalapiURL = `https://api.rentcast.io/v1/properties?${buildingURL}`;
+    console.log(externalapiURL);
+    return;
+  } catch (error) {
+    console.error("Error", error);
+    return;
+  }
+};
+
+export default { externalapifunction };
