4f31727e2cdf4f589bc1a43b1bb361c022a76dc5
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-03-27 20:05:46 -0400

    working on the datapart

--- backend/controllers/clientPref.controller.ts
+++ backend/controllers/clientPref.controller.ts
@@ -7,12 +7,12 @@ import  externalapifunction from "../services/externalapifunction.service.js";
 const clientpropdataController = async (req: Request, res: Response) => {
   // get user address data here as such
   try {
-    const address = req.body;
+    const address = { UserAddress: req.body.address };
     // research better way to validate addresses.
     if (!address) {
       throw new Error("Error not valid address");
     }
-    console.log("valid address");
+    console.log("valid address", address);
     // call the function here
     externalapifunction(address); 
     console.log('External API function called'); 
--- backend/services/externalapifunction.service.ts
+++ backend/services/externalapifunction.service.ts
@@ -9,14 +9,16 @@ const externalapifunction = async (Address: clientAddress) => {
     if (!Address) {
       throw new Error("Error no address entered");
     }
-    console.log("Address entered");
+    console.log("Address entered successfully");
     // build the external function here as such
     const buildingURL = new URLSearchParams({
       address: Address.UserAddress,
-    });
+    }); 
 
-    const externalapiURL = `https://api.rentcast.io/v1/properties?${buildingURL}`;
-    console.log(externalapiURL);
+    const address = buildingURL.get('address'); 
+
+    const externalapiURL = `https://api.rentcast.io/v1/properties?address=${address}`;
+    console.log(externalapiURL); // just to check if we are correcting getting the right URL. 
     return;
   } catch (error) {
     console.error("Error", error);
