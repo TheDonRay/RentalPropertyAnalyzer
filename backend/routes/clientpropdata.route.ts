a25b6c84739256d2a0bd64bd0debafd62ba63f20
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-04-02 20:40:45 -0400

    working on the backend

--- backend/routes/clientpropdata.route.ts
+++ backend/routes/clientpropdata.route.ts
@@ -3,6 +3,6 @@ import clientpropdataController from "../controllers/clientPref.controller.js";
 // instantiate the route here as such
 const clientpropdata = Router();
 
-clientpropdata.post("/clientpropertydata", clientpropdataController);
+clientpropdata.get("/clientpropertydata", clientpropdataController);
 
 export default clientpropdata;
